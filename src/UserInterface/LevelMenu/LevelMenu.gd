4558818c03d685b654667b1002c404682ed6fac6
aditgocendra <gocendra123@gmail.com>
2021-05-28 20:39:13 +0800

    add back button

--- src/UserInterface/LevelMenu/LevelMenu.gd
+++ src/UserInterface/LevelMenu/LevelMenu.gd
@@ -14,3 +14,7 @@ func _on_BtnLevel2_pressed():
 
 func _on_BtnLevel3_pressed():
 	get_tree().change_scene("res://src/Game/Level3.tscn")
+
+
+func _on_BackBtn_pressed():
+	get_tree().change_scene("res://src/UserInterface/MainMenu/3DSceneMenu.tscn")
